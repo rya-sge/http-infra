99e0c95573af452bb87fef07fdb518fe4fd96f40
rya-sge <ryan.sauge@heig-vd.ch>
2021-05-21 22:40:01 +0200

    Ajout script

---
+++ apache-reverse-proxy/build-image-basique.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image locally
+docker build --tag http_sauge_viotti/reverse_proxy .
---
+++ apache-reverse-proxy/run-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -p 8080:80 http_sauge_viotti/reverse-proxy
