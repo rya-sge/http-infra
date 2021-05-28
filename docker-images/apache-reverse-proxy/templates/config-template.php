a80629266edc58c6416816496db1143668b8ba38
Ryan Sauge <ryan.sauge@heig-vd.ch>
2021-05-28 17:21:56 +0200

    Ajout début load balancer

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,18 +1,38 @@
-
-
 <?php
 
-$STATIC_APP = getenv('STATIC_APP');
-$DYNAMIC_APP = getenv('DYNAMIC_APP');
+$STATIC_APP_1 = getenv('STATIC_APP_1');
+$DYNAMIC_APP_1 = getenv('DYNAMIC_APP_1');
+
+$STATIC_APP_2 = getenv('STATIC_APP_2');
+$DYNAMIC_APP_2 = getenv('DYNAMIC_APP_2');
 ?>
 
 <VirtualHost *:80>
 	ServerName incroyable.ch
 	
-	ProxyPass '/api/animals' 'http://<?php print "$DYNAMIC_APP"?>/'
-	ProxyPassReverse '/api/animals' 'http://<?php print "$DYNAMIC_APP"?>/'
+	<Proxy balancer://static>
+
+		BalancerMember http://<?php print "$STATIC_APP_1"?>/ min=10 max=500 timeout=60 				loadfactor=1
+
+		BalancerMember http://<?php print "$STATIC_APP_2"?>/ min=10 max=500 timeout=60 				loadfactor=1
+
+	</Proxy>
 	
-	ProxyPass '/' 'http://<?php print "$STATIC_APP"?>/'
-	ProxyPassReverse '/' '<?php print "$STATIC_APP"?>/'
+	<Proxy balancer://dynamic>
+		BalancerMember http://<?php print "$DYNAMIC_APP_1"?>/ min=10 max=500 timeout=60 				loadfactor=1
+
+		BalancerMember http://<?php print "$DYNAMIC_APP_2"?>/ min=10 max=500 timeout=60 				loadfactor=1
+
+	</Proxy>
+
+P	ProxyPreserveHost On
+
+	ProxyPass '/' balancer://static
+	ProxyPassReverse '/' balancer://static
+
+P	ProxyPass '/api/animals' balancer://cluster/
+
+	ProxyPassReverse '/api/animals' balancer://cluster/
+
 </VirtualHost>
 
