71c431caec424891ca5550336d399a296b04e07c
Ryan Sauge <ryan.sauge@heig-vd.ch>
2021-05-30 20:28:10 +0200

    modif script run

--- docker-images/apache-reverse-proxy/run-container.sh
+++ docker-images/apache-reverse-proxy/run-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -e STATIC_APP=172.17.0.2:80 -e STATIC_APP=172.17.0.5:80 -e DYNAMIC_APP=172.17.3.3:3000 DYNAMIC_APP=172.17.3.4:3000- -name apache_reverse http_infra_sauge_viotti/reverse_proxy
+docker run -d -e STATIC_APP_1=172.17.0.2:80 -e STATIC_APP_2=172.17.0.3:80 -e DYNAMIC_APP_1=172.17.0.4:3000 -e DYNAMIC_APP_2=172.17.0.7:3000 --name apache_reverse http_infra_sauge_viotti/reverse_proxy
 
 
 
